a2c5500334b55897a714c2b4f5507e4d0d355fbc
efarthou@gmail.com <El23291*>
2022-01-06 13:42:01 -0300

    Primeira Versão Funcional (sem teste de SAP aberto, etc)

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import auxiliares as aux
 SAP = ConectaSAP.RetornasessaoSAP('PRODUÇÃO ECC P03 - LOAD BALANCE')
 SAP.definirsessao()
 
-#if SAP.session.info.transaction == 'S000':
-SAP.login = aux.criarinputbox('Usuário', 'Digite o Login:')
-SAP.senha = aux.criarinputbox('Senha', 'Digite o Senha:', '*')
-if len(SAP.login) > 0:
-    SAP.session.findById("wnd[0]/usr/txtRSYST-BNAME").Text = SAP.login
-if len(sessao.senha) > 0:
-    SAP.session.findById("wnd[0]/usr/pwdRSYST-BCODE").Text = SAP.senha
-
-SAP.session.findById("wnd[0]").sendVKey(0)
+if SAP.session.info.transaction == 'S000':
+    SAP.login = aux.criarinputbox('Usuário', 'Digite o Login:')
+    SAP.senha = aux.criarinputbox('Senha', 'Digite o Senha:', '*')
+    if len(SAP.login) > 0:
+        SAP.session.findById("wnd[0]/usr/txtRSYST-BNAME").Text = SAP.login
+    if len(SAP.senha) > 0:
+        SAP.session.findById("wnd[0]/usr/pwdRSYST-BCODE").Text = SAP.senha
+    SAP.session.findById("wnd[0]").sendVKey(0)
